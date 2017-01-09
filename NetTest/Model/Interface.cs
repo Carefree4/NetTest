550bdb93f4c10b253f1ae2c6c84cde3071ead839
Max Wenger <carefree4@gmail.com>
2017-01-09 12:37:49 -0800

    Test

---
+++ NetTest/Model/Interface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NetTest.Model
+{
+    public class Interface : ModelBase
+    {
+        private string address;
+        private string name;
+        
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+                OnPropertyChanged(nameof(Address));
+            }
+        }
+
+    }
+}
