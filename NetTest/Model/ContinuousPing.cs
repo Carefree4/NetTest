e25151aa01852800c40e768e5467cc5c980b017c
Max Wenger <carefree4@gmail.com>
2017-01-09 12:31:24 -0800

    Fixed namespace issues and new model to define continious ping

---
+++ NetTest/Model/ContinuousPing.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using NetTest.Annotations;
+
+namespace NetTest.Model
+{
+    public class ContinuousPing : ModelBase
+    {
+        private string address;
+        private string interfaceName;
+        private string roundTripTime;
+        private string status;
+        private string timeToLive;
+
+        public string InterfaceName
+        {
+            get { return interfaceName; }
+            set
+            {
+                interfaceName = value;
+                OnPropertyChanged(nameof(InterfaceName));
+            }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+                OnPropertyChanged(nameof(Address));
+            }
+        }
+
+        public string Status
+        {
+            get { return status; }
+            set
+            {
+                status = value;
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+
+        public string RoundTripTime
+        {
+            get { return roundTripTime; }
+            set
+            {
+                roundTripTime = value;
+                OnPropertyChanged(nameof(RoundTripTime));
+            }
+        }
+
+        public string TimeToLive
+        {
+            get { return timeToLive; }
+            set
+            {
+                timeToLive = value;
+                OnPropertyChanged(nameof(TimeToLive));
+            }
+        }
+
+
+        public new string ToString()
+            => $"\"{Status}\" from {InterfaceName}({Address}) in >={RoundTripTime} with {TimeToLive} hops left.";
+    }
+}
--- NetTest/ViewModel/ContinuousPingViewModel.cs
+++ NetTest/ViewModel/ContinuousPingViewModel.cs
@@ -1,23 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using NetTest.Annotations;
+using NetTest.Model;
 using NetTest.Services.ICMP;
 
 namespace NetTest.ViewModel
 {
     public class ContinuousPingViewModel : ViewModelBase
     {
-        private PingSender pingSender;
+        private readonly PingSender pingSender;
 
         public ContinuousPingViewModel()
         {
             pingSender = new PingSender();
-            PingResponces = new ObservableCollection<PingResponceViewModel>();
+            PingResponces = new ObservableCollection<ContinuousPing>();
 
             pingSender.AllPingsReceived += (sender, args) => { UpdatePingResponces(args.PingResults); };
             PingResponces.CollectionChanged += (sender, args) => UpdatePingSenderList();
@@ -27,7 +25,7 @@ namespace NetTest.ViewModel
 
         public RelayCommand TogglePings { get; set; }
 
-        public ObservableCollection<PingResponceViewModel> PingResponces { get; set; }
+        public ObservableCollection<ContinuousPing> PingResponces { get; set; }
 
         private void UpdatePingSenderList()
         {
@@ -51,81 +49,6 @@ namespace NetTest.ViewModel
                     }
                 }
             }
-        }
-
-        public class PingResponceViewModel : INotifyPropertyChanged
-        {
-            private string address;
-            private string interfaceName;
-            private string roundTripTime;
-            private string status;
-            private string timeToLive;
-
-            public string InterfaceName
-            {
-                get { return interfaceName; }
-                set
-                {
-                    interfaceName = value;
-                    OnPropertyChanged(nameof(InterfaceName));
-                }
-            }
-
-            public string Address
-            {
-                get { return address; }
-                set
-                {
-                    address = value;
-                    OnPropertyChanged(nameof(Address));
-                }
-            }
-
-            public string Status
-            {
-                get { return status; }
-                set
-                {
-                    status = value;
-                    OnPropertyChanged(nameof(Status));
-                }
-            }
-
-            public string RoundTripTime
-            {
-                get { return roundTripTime; }
-                set
-                {
-                    roundTripTime = value;
-                    OnPropertyChanged(nameof(RoundTripTime));
-                }
-            }
-
-            public string TimeToLive
-            {
-                get { return timeToLive; }
-                set
-                {
-                    timeToLive = value;
-                    OnPropertyChanged(nameof(TimeToLive));
-                }
-            }
-
-
-            public new string ToString()
-                => $"\"{Status}\" from {InterfaceName}({Address}) in >={RoundTripTime} with {TimeToLive} hops left.";
-
-            #region INotifyPropertyChanged Members
-
-            public event PropertyChangedEventHandler PropertyChanged;
-
-            [NotifyPropertyChangedInvocator]
-            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
-
-            #endregion // INotifyPropertyChanged Members
         }
     }
 }
